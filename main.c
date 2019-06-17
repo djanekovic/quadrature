eeeac7b6c20f36bb3dd1f924e62f793397c2a85a
Darko Janekovic <darko.janekovic@fer.hr>
2019-06-17 17:46:29 +0200

    testing new functions
    Signed-off-by: Darko Janekovic <darko.janekovic@fer.hr>

--- main.c
+++ main.c
@@ -10,8 +10,20 @@ double f_1(double x, double y, double z)
 
 int main(int argc, char **argv)
 {
+    double exact = 2.0;
+    quadrature_t q;
+
     for (int i = 2; i <= 16; i++) {
-        printf("%d: int sin(x)dx from 0 to pi: %.17g\n", i,
-               compute_integral_line(f_1, 0, M_PI, i));
+        init_quadrature(i, &q);
+        printf("%d points: error = %.17g\n", i,
+                exact - compute_integral_line(f_1, 0, M_PI, &q));
+        destroy_quadrature(&q);
+    }
+
+    puts("");
+
+    for (int i = 2; i <= 16; i++) {
+        printf("%d points: error = %.17g\n", i,
+                exact - compute_integral_line_alloc(f_1, 0, M_PI, i));
     }
 }
